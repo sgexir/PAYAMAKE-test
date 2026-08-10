4cc505cd762dfadd545a2a6393eddda6130730e9
sgexir <masood.vahidi6378@gmail.com>
2026-08-11 03:22:00 +0330

    Complete initial PAYAMAKE landing page structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,78 @@
-console.log("PAYAMAKE initialized");
+const volumeSlider = document.querySelector(
+    '.pricing-slider input[type="range"]'
+);
+
+
+const volumeNumber = document.querySelector(
+    '.volume-number'
+);
+
+
+const priceNumber = document.querySelector(
+    '.price-card h3'
+);
+
+
+
+function updatePricing(){
+
+
+    let volume = Number(volumeSlider.value);
+
+
+    let formattedVolume = volume.toLocaleString('fa-IR');
+
+
+    volumeNumber.innerHTML =
+        formattedVolume + " پیامک";
+
+
+
+    let price;
+
+
+
+    if(volume < 100000){
+
+        price = 270;
+
+    }
+
+
+    else if(volume < 300000){
+
+        price = 240;
+
+    }
+
+
+    else if(volume < 1000000){
+
+        price = 220;
+
+    }
+
+
+    else {
+
+        price = "اختصاصی";
+
+    }
+
+
+
+    priceNumber.innerHTML = price;
+
+
+}
+
+
+
+volumeSlider.addEventListener(
+    "input",
+    updatePricing
+);
+
+
+
+updatePricing();
